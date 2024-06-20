52bcb3b36428e7a41bf004e04caf8e2cbd028fd5
Vale-JP <dmoreiravale@gmail.com>
2024-06-20 15:40:40 -0300

    Soluções dos exercícios

--- exercises/arrayFunctions.js
+++ exercises/arrayFunctions.js
@@ -1,16 +1,32 @@
-function dobrarValores(array) {}
+function dobrarValores(array) {
+  return array.map(num => num * 2)
+}
 
-function filtrarPares(array) {}
+function filtrarPares(array) {
+  return array.filter(num => num % 2 === 0)
+}
 
-function somarElementos(array) {}
+function somarElementos(array) { 
+  return array.reduce((num1, num2) => num1 + num2, 0)
+}
 
-function reverterArray(array) {}
+function reverterArray(array) {
+  return array.reverse()
+}
 
-function removerDuplicados(array) {}
+function removerDuplicados(array) {
+  return Array.from(new Set(array))
+}
 
-function concatenarArrays(array1, array2) {}
-
-function maiorNumero(array) {}
+function concatenarArrays(array1, array2) {
+  return array1.concat(array2)
+}
+function maiorNumero(array) {
+  if(array.length === 0){ //verifica se o array esta vazia (OBS)
+    return -Infinity
+  }
+  return Math.max(...array) //retornar o maior numero (OBS)
+}
 
 module.exports = {
   dobrarValores,
