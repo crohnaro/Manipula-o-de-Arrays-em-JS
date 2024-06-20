626d3f5e37811fe159afa8b6c11e477e5a7bcbf9
rafaelmarcx <kiba.games123@gmail.com>
2024-06-20 15:59:47 -0300

    Solução dos exercícios

--- exercises/arrayFunctions.js
+++ exercises/arrayFunctions.js
@@ -1,16 +1,38 @@
-function dobrarValores(array) {}
+function dobrarValores(array) {
+  return array.map(item => item *2)
+}
 
-function filtrarPares(array) {}
 
-function somarElementos(array) {}
+function filtrarPares(array) {
+  return array.filter(item1 => item1 *2 === 0)
+}
 
-function reverterArray(array) {}
+function somarElementos(array) {
+  return array.reduce((acumulador, valor) => acumulador + valor, 0)
+}
 
-function removerDuplicados(array) {}
+function reverterArray(array) {
+    return array.reverse()
+}
 
-function concatenarArrays(array1, array2) {}
+function removerDuplicados(array) {
+  let set = new Set(array);
+  let novoArray = Array.from(set);
+  return novoArray
+}
 
-function maiorNumero(array) {}
+function concatenarArrays(array1, array2) {
+  return array1.concat(array2)
+}
+
+function maiorNumero(array) {
+  if (array.length === 0)
+    return -Infinity
+
+  array.sort ((a,b) => a-b)
+  let lastElement = array.pop();
+  return lastElement
+}
 
 module.exports = {
   dobrarValores,
